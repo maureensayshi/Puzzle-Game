4ce51c897f5a96bfc2ce10a45bda724861c197a2
maureensayshi <maureen81711@gmail.com>
2018-09-14 17:53:49 +0800

    upload

---
+++ src/js/components/App.js
@@ -0,0 +1,30 @@
+// import React, {Component} from "react";
+// import ReactDOM from "react-dom";
+// import {BrowserRouter, Route, Link} from "react-router-dom";
+
+// const NewRoute = () => {
+//   console.log("newroute had run");  
+//   return(
+//     <div>
+//       <p>new route</p>
+//     </div>
+//   );
+// };
+
+// class App extends Component {
+  
+//   render(){
+//     console.log("app had run");
+//     return (
+//       <BrowserRouter>
+//         <Route path="/new" component={NewRoute} />
+//       </BrowserRouter>
+//     );
+//   }
+// }
+
+// export default App;
+
+// ReactDOM.render(<App />, document.querySelector(".appContainer"));
+
+// console.log("file had run");
---
+++ src/pages/Ranking/Ranking.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Ranking extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is Ranking</p>
+      </div>
+    );
+  }
+}
